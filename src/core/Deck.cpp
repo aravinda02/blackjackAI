be9aa5f450bfb85cfcb6ada5f804a4d009f60fda
aravinda02 <aravind234@hotmail.com>
2025-10-03 20:59:55 +1300

    na

---
+++ src/core/Deck.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Card.h"
+#include <vector>
+#pragma once
+
+class Deck{
+    public :
+    Deck(int numDecks=1);
+    void shuffle();
+    Card dealCard();
+    bool isEmpty() const;
+    void reset();
+
+    private:
+    std::vector<Card> cards;
+    int numDecks;
+    void initialize();            
+};
